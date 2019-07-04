9539a4fe51e97cf94f53abc76dcd6bb86b448c32
keshabchalise <keshabchalise40@gmail.com>
2019-07-04 17:57:29 +0545

    use of redux thunk middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
-export const fetchPosts = async()=>{
-    const response = await jsonPlaceHolder.get('/posts');
-    return {
-        type:'FETCH_POSTS',
-        payload:response
+export const fetchPosts = ()=>{
+
+    return async (dispatch,getState)=>{
+
+        const response = await jsonPlaceHolder.get('/posts');
+
+        dispatch({type:'FETCH_POSTS',payload:response});
     }
 }
